807020d4c5b82068e83d075f2cffa68615f04411
victus <zlotfi758@gmail.com>
2023-12-13 16:18:47 +0330

    add save method

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,4 +1,27 @@
 package repository;
 
+import connection.JdbcConnection;
+import entity.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserRepository {
+
+    JdbcConnection jdbcConnection = new JdbcConnection();
+    Connection connection = jdbcConnection.getConnection();
+
+    public UserRepository() throws SQLException {
+    }
+
+    public int save(User user) throws SQLException {
+        String add = "INSERT INTO user1 (userName, password, signup_date) VALUES (?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(add);
+        preparedStatement.setString(1, user.getUserName());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.setString(3, user.getSignup_date());
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
 }
