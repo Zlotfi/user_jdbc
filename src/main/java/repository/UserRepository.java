1a4beca45b669848ef0e1b33c0ca8f7395081bd7
victus <zlotfi758@gmail.com>
2023-12-13 17:33:55 +0330

    add delete method

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -54,4 +54,12 @@ public class UserRepository {
         int result = preparedStatement.executeUpdate();
         return result;
     }
+
+    public int delete(int id) throws SQLException {
+        String query = "DELETE FROM user1 WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1,id);
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
 }
