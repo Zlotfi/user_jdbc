e8b31ec4058478b15d320c7a1e7ea68e7a603ed9
victus <zlotfi758@gmail.com>
2023-12-13 15:58:22 +0330

    add connection field

--- src/main/java/connection/JdbcConnection.java
+++ src/main/java/connection/JdbcConnection.java
@@ -1,4 +1,17 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+    public Connection connection = DriverManager.getConnection
+            ("jdbc:postgresql://localhost:5432/postgres","postgres","zl13801393#");
+
+    public JdbcConnection() throws SQLException {
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
